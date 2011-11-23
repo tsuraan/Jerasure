808875a237fc44c65a27e28558791e8eacbeb211
jay groven <jgroven@lutsendata.com>
2011-11-22 21:56:48 -0600

    update to Jerasure 1.2A, which mostly changes the license to 3-clause BSD, and also appears to fix some bugs

--- Examples/encoder.c
+++ Examples/encoder.c
@@ -2,32 +2,49 @@
  * Catherine D. Schuman, James S. Plank
 
 Jerasure - A C/C++ Library for a Variety of Reed-Solomon and RAID-6 Erasure Coding Techniques
-Copright (C) 2007 James S. Plank
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+Revision 1.2A
+May 24, 2011
 
 James S. Plank
 Department of Electrical Engineering and Computer Science
 University of Tennessee
 Knoxville, TN 37996
 plank@cs.utk.edu
-*/
 
-/*
- * $Revision: 1.2 $
- * $Date: 2008/08/19 17:53:34 $
+Copyright (c) 2011, James S. Plank
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in
+   the documentation and/or other materials provided with the
+   distribution.
+
+ - Neither the name of the University of Tennessee nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+
  */
 
 /* 
@@ -108,7 +125,7 @@ int main (int argc, char **argv) {
 	
 	/* Creation of file name variables */
 	char temp[5];
-	char *s1, *s2;
+	char *s1, *s2, *extension;
 	char *fname;
 	int md;
 	char *curdir;
@@ -364,6 +381,7 @@ int main (int argc, char **argv) {
 		}
 	}
 
+
 	/* Determine size of k+m files */
 	blocksize = newsize/k;
 
@@ -396,13 +414,11 @@ int main (int argc, char **argv) {
 	}
 	s2 = strchr(s1, '.');
 	if (s2 != NULL) {
-		*s2 = '\0';
-	}
-	fname = strchr(argv[1], '.');
-	s2 = (char*)malloc(sizeof(char)*(strlen(argv[1])+5));
-	if (fname != NULL) {
-		strcpy(s2, fname);
-	}
+          extension = strdup(s2);
+          *s2 = '\0';
+	} else {
+          extension = strdup("");
+        }
 	
 	/* Allocate for full file name */
 	fname = (char*)malloc(sizeof(char)*(strlen(argv[1])+strlen(curdir)+10));
@@ -414,9 +430,11 @@ int main (int argc, char **argv) {
 	coding = (char **)malloc(sizeof(char*)*m);
 	for (i = 0; i < m; i++) {
 		coding[i] = (char *)malloc(sizeof(char)*blocksize);
+                if (coding[i] == NULL) { perror("malloc"); exit(1); }
 	}
 
 	
+
 	/* Create coding matrix or bitmatrix and schedule */
 	gettimeofday(&t3, &tz);
 	switch(tech) {
@@ -458,6 +476,7 @@ int main (int argc, char **argv) {
 	tsec -= t3.tv_sec;
 	totalsec += tsec;
 	
+
 	/* Read in data until finished */
 	n = 1;
 	total = 0;
@@ -520,7 +539,7 @@ int main (int argc, char **argv) {
 			if (fp == NULL) {
 				bzero(data[i-1], blocksize);
  			} else {
-				sprintf(fname, "%s/Coding/%s_k%0*d%s", curdir, s1, md, i, s2);
+				sprintf(fname, "%s/Coding/%s_k%0*d%s", curdir, s1, md, i, extension);
 				if (n == 1) {
 					fp2 = fopen(fname, "wb");
 				}
@@ -536,7 +555,7 @@ int main (int argc, char **argv) {
 			if (fp == NULL) {
 				bzero(data[i-1], blocksize);
  			} else {
-				sprintf(fname, "%s/Coding/%s_m%0*d%s", curdir, s1, md, i, s2);
+				sprintf(fname, "%s/Coding/%s_m%0*d%s", curdir, s1, md, i, extension);
 				if (n == 1) {
 					fp2 = fopen(fname, "wb");
 				}
@@ -573,7 +592,6 @@ int main (int argc, char **argv) {
 
 
 	/* Free allocated memory */
-	free(s2);
 	free(s1);
 	free(fname);
 	free(block);
