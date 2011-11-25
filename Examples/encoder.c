f6b23bc2a64f3da3cdc0a255e0d62db51bb44b6e
tpltnt <tpltnt.github@dropcut.net>
2011-11-25 23:59:24 +0100

    conversion specifier fixed

--- Examples/encoder.c
+++ Examples/encoder.c
@@ -626,7 +626,7 @@ int is_prime(int w) {
 void ctrl_bs_handler(int dummy) {
 	time_t mytime;
 	mytime = time(0);
-	fprintf(stderr, "\n%d\n", ctime(&mytime));
+	fprintf(stderr, "\n%s\n", ctime(&mytime));
 	fprintf(stderr, "You just typed ctrl-\\ in encoder.c.\n");
 	fprintf(stderr, "Total number of read ins = %d\n", readins);
 	fprintf(stderr, "Current read in: %d\n", n);
